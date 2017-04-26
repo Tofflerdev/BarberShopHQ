06d18c33f7472ac0f19742b70ac55820bd6a1d57
TofflerDev <baraban.ms@gmail.com>
2017-04-26 22:15:50 +0300

    add layout & :visit

--- app.rb
+++ app.rb
@@ -12,3 +12,13 @@ end
 get '/' do
 	erb "Hello!"			
 end
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date = params[:date]
+	erb "#{@username} we'll be waiting for you at #{@date}"
+end
